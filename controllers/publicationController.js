de87fc82413acf92d2c06a20376164ca27c98cca
GabrielsantiagoRamirez <gabrielsabtiago716@gmail.com>
2025-01-23 19:56:43 -0500

    fix create suggestion and suggestionCOntroller

--- controllers/publicationController.js
+++ controllers/publicationController.js
@@ -2,7 +2,7 @@
 const fs = require("fs");
 const path = require("path");
 const cloudinary = require('../config/cloudinary-config');
-const Suggestions = require('../models/suggestions');
+const Suggestion = require('../models/suggestions');
 
 
 //Importar modelos

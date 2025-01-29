80d1b992a0aebdc564cb1130624a54f8bf46a5ea
GabrielsantiagoRamirez <gabrielsabtiago716@gmail.com>
2025-01-28 19:37:19 -0500

    Add endpoint baDate hjave changes

--- routes/agendaRoutes.js
+++ routes/agendaRoutes.js
@@ -4,7 +4,7 @@ const agendaController = require('../controllers/agendaController');
 const check = require('../middlewares/auth');
 
 router.post("/save", check.auth, agendaController.save);
-router.get("/byDate", check.auth, agendaController.save);
+router.get("/byDate/:date", check.auth, agendaController.save);
 
 
 module.exports = router;

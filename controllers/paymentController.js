46d274d339799610d2b79b26afd763ed821efb8d
GabrielsantiagoRamirez <gabrielsabtiago716@gmail.com>
2025-01-29 15:29:26 -0500

    add new endpoints for payment

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -10,7 +10,7 @@ const createPayment = async (req, res) => {
         const paymentIntent = await createPaymentIntent(amount, currency, paymentMethodId);
 
         if (!paymentIntent) {
-            return res.status(400).json({ error: "No se pudo crear el pago en Stripe" });
+            return res.status(400).json({ error: "Payment couldn't be created in Stripe" });
         }
 
         // 2️⃣ Guardar el pago en MongoDB
@@ -24,10 +24,55 @@ const createPayment = async (req, res) => {
 
         await payment.save();
 
-        res.status(201).json({ message: "Pago exitoso", payment });
+        res.status(201).json({ message: "Successful payment", payment });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 };
 
-module.exports = { createPayment };
+const myPayments = async (req, res) => {
+    try {
+        const userId = req.user.id; // Extraído desde el token
+
+        // Buscar todos los pagos del usuario logueado
+        const payments = await Payment.find({ userId });
+
+        if (!payments || payments.length === 0) {
+            return res.status(404).json({ message: "No payments found for this user" });
+        }
+
+        res.status(200).json({ payments });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+
+const allPayments = async (req, res) => {
+    try {
+        const { page = 1, limit = 10 } = req.query; // Parámetros de paginación
+
+        const options = {
+            page: parseInt(page),
+            limit: parseInt(limit),
+            sort: { createdAt: -1 } // Ordenar por fecha de creación (más reciente primero)
+        };
+
+        // Buscar todos los pagos con paginación
+        const payments = await Payment.paginate({}, options);
+
+        if (!payments || payments.docs.length === 0) {
+            return res.status(404).json({ message: "No payments found" });
+        }
+
+        res.status(200).json({ payments });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+module.exports = { 
+    createPayment,
+    myPayments,
+    allPayments
+};
--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -4,5 +4,7 @@ const paymentController = require("../controllers/paymentController");
 const check = require('../middlewares/auth');
 
 router.post("/payment",check.auth, paymentController.createPayment);
+router.get('/myPayments', check.auth, paymentController.myPayments);
+router.get('/admin/payments',  check.auth, paymentController.allPayments);
 
 module.exports = router;

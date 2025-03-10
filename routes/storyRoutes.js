654e04df6fd4a63e21d96102dba03ff74887419e
GabrielsantiagoRamirez <gabrielsabtiago716@gmail.com>
2025-03-09 19:33:56 -0500

    Modify for vercel

--- routes/storyRoutes.js
+++ routes/storyRoutes.js
@@ -5,7 +5,7 @@ const storyController = require('../controllers/storyController');
 const check = require("../middlewares/auth");
 
 // Configuración de subida temporal
-const uploads = multer({ dest: 'temp/' });
+const uploads = multer({ storage: multer.memoryStorage() });
 
 router.post("/upload", [check.auth, uploads.single("file0")], storyController.upload);
 router.get("/allStories", check.auth, storyController.allStories);
